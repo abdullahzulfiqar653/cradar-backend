721869eadc79fdaeee8bddb68980ee189c472fdb
Leaw Jia Ning <leaw_ning@hotmail.com>
2024-03-07 19:41:27 +0800

    mkdir credentials

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ ! -f "credentials/sylvan-harmony-404807-02d7953b3a5c.json" ] \
     && [ -n "${GOOGLE_APPLICATION_CREDENTIALS_JSON}" ]; then
+    mkdir -p credentials
     echo $GOOGLE_APPLICATION_CREDENTIALS_JSON > credentials/sylvan-harmony-404807-02d7953b3a5c.json
 fi
 
