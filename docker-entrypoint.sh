a86d92300a5265cfbe448b63145df12c33add1d8
Leaw Jia Ning <leaw_ning@hotmail.com>
2024-03-07 22:12:55 +0800

    Attempt to fix the echo again

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-echo $GOOGLE_APPLICATION_CREDENTIALS_JSON
-
 if [ ! -f "credentials/sylvan-harmony-404807-02d7953b3a5c.json" ] \
     && [ -n "${GOOGLE_APPLICATION_CREDENTIALS_JSON}" ]; then
     mkdir -p credentials
-    echo $GOOGLE_APPLICATION_CREDENTIALS_JSON > credentials/sylvan-harmony-404807-02d7953b3a5c.json
+    echo -E $GOOGLE_APPLICATION_CREDENTIALS_JSON > credentials/sylvan-harmony-404807-02d7953b3a5c.json
 fi
 
 python manage.py migrate
