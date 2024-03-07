b1d9d2870ce8f4d9b6fe5d75a102bb3b1008d435
Leaw Jia Ning <leaw_ning@hotmail.com>
2024-03-07 19:21:58 +0800

    Add google application credentials file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ ! -f "credentials/sylvan-harmony-404807-02d7953b3a5c.json" ] \
+    && [ -n "${GOOGLE_APPLICATION_CREDENTIALS_JSON}" ]; then
+    cat $GOOGLE_APPLICATION_CREDENTIALS_JSON > credentials/sylvan-harmony-404807-02d7953b3a5c.json
+fi
+
 python manage.py migrate
 
 python manage.py loaddata api/fixtures/*
